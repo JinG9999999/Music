62500b5252cc03c3254e7a2c05e7e83013bda39a
吕静 <@LLL-JJ>
2020-04-13 14:24:40 +0800

    添加项目文件。

---
+++ Music.HC/Controllers/musicController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Music.HC.Models;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Music.HC.Controllers
+{
+    public class musicController : Controller
+    {
+        // GET: music
+        HttpClientHelper hc = new HttpClientHelper("http://localhost:57338/");
+        public ActionResult Show()
+        {
+            bandsel();
+            var json = hc.Get("api/music");
+            var list = JsonConvert.DeserializeObject<PagedInfo>(json);
+            return View(list);
+        }
+        [HttpPost]
+        public ActionResult Show(string Name, string Cid, int currentPage = 1)
+        {
+            bandsel();
+            var json = hc.Get(string.Format("api/music?Name={0}&Cid={1}&index={2}", Name, Cid, currentPage));
+            var list = JsonConvert.DeserializeObject<PagedInfo>(json);
+            return View(list);
+        }
+        public void bandsel()
+        {
+            var json = hc.Get("api/corp");
+            var list = JsonConvert.DeserializeObject<List<CorpModel>>(json);
+            ViewBag.bandsel = new SelectList(list, "Cid", "CorpName");
+        }
+        public ActionResult Add()
+        {
+            bandsel();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Add(MusicModel m, HttpPostedFileBase Cover)
+        {
+            m.Adder = Session["name"].ToString();
+            if (Cover != null)
+            {
+                string path = "/Content/imgs/" + Path.GetFileName(Cover.FileName);
+                Cover.SaveAs(Server.MapPath(path));
+                m.Cover = path;
+            }
+            m.Tags = Request["Tags"];
+            if (hc.Post("api/music", JsonConvert.SerializeObject(m)) == "1")
+            {
+                return Content("<script>alert('添加成功');location.href='/music/Show';</script>");
+            }
+            else
+            {
+                Response.Write("<script>alert('添加失败');</script>");
+            }
+            return View(m);
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(UserModel m)
+        {
+            if (hc.Post("api/user", JsonConvert.SerializeObject(m)) == "1")
+            {
+                Session["name"] = m.UserName;
+                return Content("<script>alert('登陆成功');location.href='/music/Show'</script>");
+            }
+            else
+            {
+                return Content("<script>alert('登录失败');</script>");
+            }
+
+        }
+
+        public ActionResult Upt(int id)
+        {
+            bandsel();
+            var json = hc.Get("api/music/" + id);
+            var list = JsonConvert.DeserializeObject<MusicModel>(json);
+            return View(list);
+        }
+        [HttpPost]
+        public ActionResult Upt(MusicModel m, HttpPostedFileBase Cover)
+        {
+            if (Cover != null)
+            {
+                string path = "/Content/imgs/" + Path.GetFileName(Cover.FileName);
+                Cover.SaveAs(Server.MapPath(path));
+                m.Cover = path;
+            }
+            m.Tags = Request["Tags"];
+            if (hc.Put("api/music", JsonConvert.SerializeObject(m)) == "1")
+            {
+                return Content("<script>alert('修改成功');location.href='/music/Show'</script>");
+            }
+            else
+            {
+                Response.Write("<script>alert('修改失败');</script>");
+            }
+            return View(m);
+        }
+        public ActionResult Del(string id)
+        {
+            if (hc.Delete("api/music/" + id) == "1")
+            {
+                return Content("<script>alert('删除成功');location.href='/music/Show'</script>");
+            }
+            else
+            {
+                return Content("<script>alert('删除失败');</script>");
+            }
+
+        }
+
+        public ActionResult XQ(string id)
+        {
+            bandsel();
+            var json = hc.Get("api/music/" + id);
+            var list = JsonConvert.DeserializeObject<MusicModel>(json);
+            return View(list);
+        }
+    }
+}
---
+++ Music.HC/Models/MusicModel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Music.HC.Models
+{
+    public class MusicModel //唱片表
+    {
+        [Key]
+        [Display(Name = "编号")]
+        [Required]
+        public int Mid { get; set; }
+        [Display(Name = "音乐名称")]
+        [Required]
+        public string CorpName { get; set; } //音乐名称
+        [Display(Name = "唱片封面")]
+        [Required]
+        public string Cover { get; set; } //唱片封面
+        [Display(Name = "唱片标签")]
+        [Required]
+        public string Tags { get; set; } //唱片标签
+        [Display(Name = "制作人")]
+        [Required]
+        public string Maker { get; set; } //制作人
+        [Display(Name = "演唱")]
+        [Required]
+        public string Singer { get; set; } //演唱
+        [Display(Name = "音乐公司")]
+        [Required]
+        public int CorpModelCid { get; set; } //音乐公司 外键
+        [Display(Name = "发布日期")]
+        [Required]
+        public DateTime ReleaseDate { get; set; } //发布日期
+        [Display(Name = "价格")]
+        [Required]
+        public int Price { get; set; } //价格
+        [Display(Name = "添加人姓名")]
+        [Required]
+        public string Adder { get; set; } //添加人姓名
+        [Display(Name = "唱片详情")]
+        [Required]
+        public string CdDetail { get; set; } //唱片详情
+        public virtual CorpModel CorpModels { get; set; }
+    }
+    public class CorpModel //唱片公司表
+    {
+        [Key]
+        public int Cid { get; set; }
+        [Display(Name = "音乐公司")]
+        [Required]
+        public string CorpName { get; set; } //音乐公司
+        [Display(Name = "公司地址")]
+        [Required]
+        public string Location { get; set; } //公司地址
+    }
+
+    public class UserModel //登录表
+    {
+        [Key]
+        public int Uid { get; set; }
+        [Display(Name = "用户姓名")]
+        [Required]
+        public string UserName { get; set; }
+        [Display(Name = "用户密码")]
+        [Required]
+        public string Password { get; set; }
+    }
+
+    public class PagedInfo
+    {
+        public List<MusicModel> musics { get; set; }
+        public int TotalConut { get; set; }   //记录条数
+        public int TotalPage { get; set; }    //总页数
+        public int currentPage { get; set; }  //当前页
+    }
+}
